c32f7dde3e0a71f2bfc27a55e6ad833b264485d6
EC2 Default User <ec2-user@ip-172-16-76-183.ap-southeast-1.compute.internal>
2023-03-06 15:26:54 +0000

    Add LGMB training

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
+# modify this accordingly
 def clean(df):
-    df["quality"] = df["quality"] - 3
     return df
 
 if __name__ == "__main__":
+    target = 'Strength'
+    rejected_features = ['id']
+
     # read the training data
-    train = pd.read_csv('data/raw/train/train.csv').drop('Id',axis=1)
+    train = pd.read_csv('data/raw/train/train.csv').drop(rejected_features, axis=1)
 
     # combine with original training set
     orig_train = pd.read_csv('data/raw/train/orig_train.csv')
@@ -14,7 +17,7 @@ if __name__ == "__main__":
     train = pd.concat([train, orig_train]).reset_index(drop=True)
 
     # read the test data
-    test = pd.read_csv('data/raw/test/test.csv').drop('Id',axis=1)
+    test = pd.read_csv('data/raw/test/test.csv').drop(rejected_features, axis=1)
 
     train = clean(train)
 
